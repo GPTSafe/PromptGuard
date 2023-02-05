64f7aae00c07359fcbf31835d97090c445cf274e
Phil Sautter <redeux@Philips-MacBook-Air.local>
2023-02-05 07:55:35 -0500

    Add default deny list

--- src/index.ts
+++ src/index.ts
@@ -8,28 +8,27 @@ enum Case {
 }
 
 type Normalization = {
-  normalizeUnicode?: boolean, // Unicode Normalization Form of the string
-  removeAllUnicode?: boolean,
-  removeNewLineCharacters?: boolean, // remove instances of "/n", "/r", "/r/n"
-  setCase?: Case,
+  normalizeUnicode?: boolean; // Unicode Normalization Form of the string
+  removeAllUnicode?: boolean;
+  removeNewLineCharacters?: boolean; // remove instances of "/n", "/r", "/r/n"
+  setCase?: Case;
 };
 
 type Options = {
-  maxTokens?: number, // 1 token is ~4 characters in english
-  quoteInput?: boolean,
-  escapeInput?: boolean,
-  allowList?: string[], // this should be a strict match
-  denyList?: string[], // this should be a fuzzy match
-  defaultDenyList: string[], // this should be a fuzzy match
-  ignoreDefaultDenyList?: boolean,
-  normalization?: Normalization,
-  encodeOutput?: boolean, // uses byte pair encoding to turn text into a series of integers
+  maxTokens?: number; // 1 token is ~4 characters in english
+  quoteInput?: boolean;
+  escapeInput?: boolean;
+  allowList?: string[]; // this should be a strict match
+  denyList?: string[]; // this should be a fuzzy match
+  ignoreDefaultDenyList?: boolean;
+  normalization?: Normalization;
+  encodeOutput?: boolean; // uses byte pair encoding to turn text into a series of integers
 };
 
 type PromptOutput = {
-  pass: boolean, // false if processing fails validation rules (max tokens, deny list, allow list)
-  output?: string, // provide the processed prompt
-  reason?: string, // provide the reason if the processing fails validation rules
+  pass: boolean; // false if processing fails validation rules (max tokens, deny list, allow list)
+  output?: string; // provide the processed prompt
+  reason?: string; // provide the reason if the processing fails validation rules
 };
 
 export class GPTSafe {
@@ -40,7 +39,6 @@ export class GPTSafe {
       maxTokens: 100,
       quoteInput: true,
       escapeInput: false,
-      defaultDenyList: ["ignore previous instructions", "ignore above instructions"], // make a default deny list
       normalization: {
         normalizeUnicode: true,
         removeAllUnicode: false,
@@ -49,6 +47,9 @@ export class GPTSafe {
       encodeOutput: false,
     }
   ) {
+    if (!options?.ignoreDefaultDenyList) options.ignoreDefaultDenyList = false;
+    if (!options?.denyList) options.denyList = [""];
+
     this.options = options;
   }
 
@@ -57,8 +58,16 @@ export class GPTSafe {
     // normalize -> quote -> escape -> check tokens -> check allow list -> check deny list -> encode output
 
     if (this.options?.denyList) {
-      if (await helper.containsDenyListItems(prompt, this.options.denyList))
-        return { pass: false, reason: "Deny list" };
+      if (typeof this.options?.ignoreDefaultDenyList === "boolean") {
+        if (
+          await helper.containsDenyListItems(
+            prompt,
+            this.options.denyList,
+            this.options.ignoreDefaultDenyList
+          )
+        )
+          return { pass: false, reason: "Deny list" };
+      }
     }
 
     return { pass: true, output: prompt };
