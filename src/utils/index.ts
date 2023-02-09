ba56eaf07d116c6568ace3361c1f22dcc19df293
Phil Sautter <20444474+redeux@users.noreply.github.com>
2023-02-09 10:12:15 -0500

    Refactor exports

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,10 +1,3 @@
 export { containsDenyListItems } from "./denylist";
-// export const promptContainsDenyListItems = containsDenyListItems;
-
 export { containsKnownAttack } from "./attackmitigation";
-// export const promptContainsKnownAttack = containsKnownAttack;
-
-// const encoder = require("./encoder");
 export { countTokens, encode } from './encoder'
-// export const countPromptTokens = countTokens;
-// export const encodePromptOutput = encode;
